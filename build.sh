512f7f20dfa4d800357a4985b9d63e98dac4e83e
徐平茂 <578749341@qq.com>
2018-02-20 18:06:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,26 @@
-pushd .
+prog=python
 
-cd src/python
+case $1 in
+    "mp" )
+    echo "use minipy to compile compiler"
+    mv minipy src/python/mp
+    prog=./mp
+    ;;
+esac
+
+pushd src/python
 rm ../cache_bin.c
 
-python encode.py init.py >> cache_bin.c
-python encode.py lex.py >> cache_bin.c
-python encode.py parse.py >> cache_bin.c
-python encode.py tmcode.py >> cache_bin.c
-python encode.py encode.py >> cache_bin.c
-python encode.py pyeval.py >> cache_bin.c
-python encode.py repl.py >> cache_bin.c
+echo "current path: $(pwd)"
+echo "use python interpreter: $prog"
+
+$prog encode.py init.py >> cache_bin.c
+$prog encode.py lex.py >> cache_bin.c
+$prog encode.py parse.py >> cache_bin.c
+$prog encode.py tmcode.py >> cache_bin.c
+$prog encode.py encode.py >> cache_bin.c
+$prog encode.py pyeval.py >> cache_bin.c
+$prog encode.py repl.py >> cache_bin.c
 mv cache_bin.c ../
 popd
 
