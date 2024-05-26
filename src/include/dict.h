6dc2bc45c173a56629b20567de25c19a26c78844
xupingmao <xupingmao@gmail.com>
2024-05-27 00:45:05 +0800

    规范代码

--- src/include/dict.h
+++ src/include/dict.h
@@ -2,8 +2,8 @@
  * @Author: xupingmao
  * @email: 578749341@qq.com
  * @Date: 2023-12-07 23:04:49
- * @LastEditors: xupingmao 578749341@qq.com
- * @LastEditTime: 2024-04-14 17:20:56
+ * @LastEditors: xupingmao
+ * @LastEditTime: 2024-05-26 23:33:25
  * @FilePath: /minipy/src/include/dict.h
  * @Description: 描述
  */
@@ -27,25 +27,25 @@ void dict_free(MpDict* dict);
 void dict_free_internal(MpDict* dict);
 void dict_print_debug_info(MpDict* dict);
 
-
 /* dict_get* functions */
-DictNode*        dict_get_node(MpDict* dict, MpObj key);
-MpObj*           dict_get_by_cstr(MpDict* dict, char* key);
-DictNode*        dict_get_node_by_index(MpDict* dict, int index);
+DictNode* dict_get_node(MpDict* dict, MpObj key);
+MpObj* dict_get_by_cstr(MpDict* dict, char* key);
+DictNode* dict_get_node_by_index(MpDict* dict, int index);
 MpObj* dict_get_by_str(MpObj obj, char* key);
 
 /* dict_set* functions */
 int dict_set0(MpDict* dict, MpObj key, MpObj val);
 void dict_set_by_cstr(MpDict* dict, const char* key, MpObj val);
 
+MpObj dict_keys(MpDict*);
 
-MpObj dict_keys(MpDict* );
+void dict_del(MpDict* dict, MpObj k);
+MpObj dict_pop(MpDict* dict, MpObj key, MpObj* _default);
 
-void             dict_del(MpDict* dict, MpObj k);
-void             MpDict_InitMethods();
+void MpDict_InitMethods();
 /** dict methods **/
-MpObj            dict_iter_new(MpObj dict);
-MpObj*           dict_next(MpData* iterator);
+MpObj dict_iter_new(MpObj dict);
+MpObj* dict_next(MpData* iterator);
 size_t dict_sizeof(MpDict* dict);
 
 #endif
